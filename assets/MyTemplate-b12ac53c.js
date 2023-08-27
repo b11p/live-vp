1c9d9da0cb2f95e640c06ba7364e8c5d44377b33
b11p <b11p@users.noreply.github.com>
2023-08-27 07:34:23 +0000

    deploy: 6599896ec379559ed09014aff2d92a039506797e

---
+++ assets/MyTemplate-b12ac53c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./app-83ed7c7c.js";const a={};function n(o,r){return t(),c("h1",null,"MyTemplate")}const l=e(a,[["render",n],["__file","MyTemplate.vue"]]);export{l as default};

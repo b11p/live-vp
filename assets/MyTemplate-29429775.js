c8d6f3f2bc349ed2546ffb3ff5ed2d84b41bcd52
b11p <b11p@users.noreply.github.com>
2023-08-27 06:36:23 +0000

    deploy: 7495cc678b3ba9e35f6c8941dca10b5bb30ad12d

---
+++ assets/MyTemplate-29429775.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./app-bca8a2e1.js";const a={};function n(o,r){return t(),c("h1",null,"MyTemplate")}const l=e(a,[["render",n],["__file","MyTemplate.vue"]]);export{l as default};
--- assets/index.html-1bf3a6f2.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Hello VuePress","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1693113181000,"contributors":[{"name":"bleatingsheep","email":"bleatingsheep@live.cn","commits":1}]},"filePathRelative":"README.md"}');export{e as data};

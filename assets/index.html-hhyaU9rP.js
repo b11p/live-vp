09502fb29efb73fe5d39cb937822c296c6919842
b11p <b11p@users.noreply.github.com>
2024-07-28 16:29:27 +0000

    deploy: 82c887da5e6535bb31a58690970328dfa8595bd0

---
+++ assets/index.html-hhyaU9rP.js
@@ -0,0 +1,1 @@
+import{_ as r,r as a,o as t,c as o,a as n,b as e,d as s}from"./app-9RzY9ATk.js";const d={},i=e("h1",{id:"crychic",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#crychic","aria-hidden":"true"},"#"),s(" CRYCHIC")],-1);function _(h,l){const c=a("Crychic");return t(),o("div",null,[i,n(c)])}const m=r(d,[["render",_],["__file","index.html.vue"]]);export{m as default};
